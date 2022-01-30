d6aabfe20def5a58e101d71dbbfcfcdfd043fb26
Adam <adamwhite930@gmail.com>
2022-01-30 19:04:02 +0000

    hw completed, could do a lot more on the design but ran out of time/inspiration/motivation

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,6 +1,6 @@
 from flask import render_template, request
 from app import app
-from modules.player_list import player_list, player1, player2
+from modules.player_list import player_list, player1, player2, player_hobbies
 from modules.player import Player
 from modules.game import Game
 import random
@@ -16,7 +16,12 @@ def game_start():
     player1.name = request.form["player1_name"].capitalize()
     print(player1.name)
     player2.name = random.choice(player_list)
-    return render_template('game.html', player1=player1, player2=player2)
+    if len(player2.hobbies) < 3:
+        for i in range(3):
+            player2.hobbies.append(random.choice(player_hobbies))
+    win_streak = random.randrange(-10, 11)
+
+    return render_template('game.html', player1=player1, player2=player2, win_streak=win_streak)
 
 @app.route('/result', methods=['GET', 'POST'])
 def play_game():
--- modules/player.py
+++ modules/player.py
@@ -3,6 +3,7 @@ from shutil import move
 
 class Player:
 
-    def __init__(self, name, move):
+    def __init__(self, name, move, hobbies):
         self.name = name
         self.move = move
+        self.hobbies = hobbies
